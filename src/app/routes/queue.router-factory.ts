eba1ba04efe60abc9c11578e9954680520d9e1c0
Freek Mencke <info@toxsickproductions.com>
2019-02-10 19:08:01 +0100

    expanded the query router

--- src/app/routes/queue.router-factory.ts
+++ src/app/routes/queue.router-factory.ts
@@ -1,45 +1,99 @@
 import { Application, Router } from 'express';
-import { Db } from 'mongodb';
+import { Db, InsertWriteOpResult, InsertOneWriteOpResult } from 'mongodb';
 import { RouterFactory } from './router.interface';
 
 export class QueueRouterFactory implements RouterFactory {
+
+  private readonly DEFAULT_MESSAGE_EXPIRY: number = 300;
 
   create(app: Application, db: Db): void {
     const router = Router();
 
     this.push(router, db);
     this.pop(router, db);
+    this.popAmount(router, db);
     this.length(router, db);
 
     app.use('/queue', router);
   }
 
   private push(router: Router, db: Db): void {
-    router.get('/push', (req, res) => {
-      db.collection('queue')
-        .insertOne({ payload: req.body })
+    router.post('/push', (req, res) => {
+      let insertPromise: Promise<InsertWriteOpResult | InsertOneWriteOpResult>;
+      if (req.body instanceof Array) {
+        insertPromise = db.collection('queue')
+          .insertMany(req.body.map(item => ({ payload: item })));
+      } else {
+        insertPromise = db.collection('queue')
+          .insertOne({ payload: req.body });
+      }
+      insertPromise
         .then(({ insertedCount }) => res.status(insertedCount > 0 ? 201 : 500).send())
         .catch(() => res.status(500).send());
     });
   }
 
-  private pop(router: Router, db: Db): void {
-    const timeout = new Date();
-    timeout.setSeconds(timeout.getSeconds() + 300); // #TODO: move to config.
+  private getExpiryDate(expiresIn?: number): Date {
+    expiresIn = expiresIn || this.DEFAULT_MESSAGE_EXPIRY;
+    const expiryDate = new Date();
+    expiryDate.setSeconds(expiryDate.getSeconds() + expiresIn);
+    return expiryDate;
+  }
 
-    router.get('/pop', (req, res) => {
-      db.collection('queue')
+  private pop(router: Router, db: Db): void {
+    router.get('/pop', async (req, res) => {
+      await db.collection('queue')
         .findOneAndUpdate(
           {
-            $or: [{ timeout: null }, { timeout: { $lte: new Date() } }]
+            $or: [{ expiryDate: null }, { expiryDate: { $lte: new Date() } }]
           },
           {
-            $set: { timeout },
+            $set: { expiryDate: this.getExpiryDate(req.query.expiresIn) },
             $inc: { tries: 1 }
           }
         )
-        .then(({ value }) => res.status(200).json(value))
+        .then(({ value }) => res.status(value ? 200 : 204).send(value))
         .catch(() => res.status(500).send());
+    });
+  }
+
+  private popAmount(router: Router, db: Db): void {
+    router.get('/pop/:amount', async (req, res) => {
+      try {
+        const items = await db.collection('queue')
+          .find({
+            $or: [
+              { expiryDate: null },
+              { expiryDate: { $lte: new Date() } }
+            ]
+          })
+          .limit(Number(req.params.amount || 1)).toArray();
+
+        await db.collection('queue')
+          .updateMany(
+            {
+              _id: { $in: items.map(item => item._id) },
+              $or: [{ expiryDate: null }, { expiryDate: { $lte: new Date() } }]
+            },
+            {
+              $set: { expiryDate: this.getExpiryDate(req.query.expiresIn)  },
+              $inc: { tries: 1 }
+            });
+
+        const results = await db.collection('queue')
+          .find({
+            _id: { $in: items.map(item => item._id) }
+          })
+          .limit(Number(req.params.amount || 1)).toArray();
+
+        if (results && results.length > 0) {
+          res.status(200).send(results);
+        } else {
+          res.status(204).send([]);
+        }
+      } catch (e) {
+        res.status(500).send();
+      }
     });
   }
 

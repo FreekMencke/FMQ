2909cb6ab7ec945e07f6fdd76ab339d3c5a04c7b
Freek Mencke <freek_mencke@outlook.com>
2022-10-02 16:54:53 +0200

    chore(swagger): improve swagger.js
    better rebuild triggers for swagger nodemon + package.json info in swagger

--- scripts/swagger.js
+++ scripts/swagger.js
@@ -1,13 +1,24 @@
+const packageJson = require('../package.json')
+
 const express = require('express');
 const swaggerAutogen = require('swagger-autogen')();
 const { serve, setup } = require('swagger-ui-express');
 
 const outputFile = './dist/swagger-output.json';
-const endpointsFiles = ['./src/app/routes/health.router-factory.ts','./src/app/routes/queue.router-factory.ts'];
+const endpointsFiles = [
+  './src/app/routes/health.router-factory.ts',
+  './src/app/routes/queue.router-factory.ts'
+];
 
 const app = express();
 
-swaggerAutogen(outputFile, endpointsFiles, { host: 'localhost:8080' }).then(({ data }) => {
+swaggerAutogen(outputFile, endpointsFiles, {
+  host: 'localhost:8080',
+  info: {
+    title: packageJson.name,
+    version: packageJson.version,
+  }
+}).then(({ data }) => {
   app.use('/', serve, setup(data));
 });
 
